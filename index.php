e040fd3a5683913e4986b5fb7230f48fdb283951
yukino <a10833354@mail.nuk.edu.tw>
2022-03-02 16:33:11 +0800

    homework1

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+
+</head>
+<body>
+    <?php
+
+    $a=10;
+    $b=20;
+
+    echo "<b>hello &nbsp world</b>";
+    echo $a*$b;
+    // echo date("y,m,d");
+    ?>
+
+</body>
+</html># web-homework
