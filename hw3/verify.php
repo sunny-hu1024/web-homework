0d3f439148c8350153f07ea8e537d0ab8a08e3a7
yahan <77716508+sunny-hu1024@users.noreply.github.com>
2022-03-31 23:28:30 +0800

    Update verify.php

--- hw3/verify.php
+++ hw3/verify.php
@@ -1,9 +1,30 @@
 <?php
 	
-	$data = new reginfo;
+	#default id and password
+	$id = 123;
+	$pwd = 123;
 
 	$log_id = $_POST['logid'];
 	$log_pw = $_POST['logpwd'];
-	$login = false;
+    $rem = $_POST['remember'];
+
+//	$cookie_on = false;
+//	if($rem == "true")
+//		$cookie_on = true;
+	
+	if($log_id==$id && $pwd==$log_pw){
+		
+		echo "<script> {window.alert('登入成功');location.href='schedule.php'} </script>";
+		#if user want to remember his/her data
+//		if($cookie_on == true){
+//			
+//		}
+		
+	}
+	else{
+		
+		echo "<script> {window.alert('登入失敗');location.href='home.html'} </script>";
+	}
 	
 ?>
+
